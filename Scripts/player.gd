9d6206e2fe377429ff8f16267d7b802f6ef11a90
KaleeBurchett <163896320+KaleeBurchett@users.noreply.github.com>
2025-11-13 10:42:30 -0500

    Add Main Files

---
+++ Scripts/player.gd
@@ -0,0 +1,28 @@
+extends CharacterBody2D
+
+const SPEED = 100.0
+
+var input_vector = Vector2.ZERO
+
+@onready var animation_tree: AnimationTree = $AnimationTree
+@onready var playback = animation_tree.get("parameters/StateMachine/playback") as AnimationNodeStateMachinePlayback
+
+func _physics_process(delta: float) -> void:
+	if playback != null:
+		var state = playback.get_current_node()
+		match state:
+			"MoveState": move_state(delta)
+			"AttackState": pass
+
+func move_state(delta: float) -> void:
+	if playback != null:
+		input_vector = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+		
+		if input_vector != Vector2.ZERO:
+			animation_tree.set("parameters/StateMachine/MoveState/RunState/blend_position", input_vector)
+
+		if Input.is_action_just_pressed("attack"):
+			playback.travel("AttackState")
+
+		velocity = input_vector * SPEED
+		move_and_slide()
