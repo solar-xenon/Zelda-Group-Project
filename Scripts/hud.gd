6cce7a7aebc8de0b8d4ba7ae02aec34eafb42314
Olivia Bertinelli <obertinelli@ivytech.edu>
2025-11-19 20:23:10 -0600

    Deleted key tracker from HUD

--- Scripts/hud.gd
+++ Scripts/hud.gd
@@ -12,17 +12,10 @@ func _ready():
 
 func _process(delta: float) -> void:
 	update_kills()
-	update_key()
 	update_medal()
 
 func update_kills():
 	$VBoxContainer/KillCount.text = "Enemies Killed: " + str(CollectionManager.enemies_killed)
-
-func update_key():
-	if CollectionManager.has_key:
-		$VBoxContainer/HasKey.text = "Key: Yes"
-	else:
-		$VBoxContainer/HasKey.text = "Key: No"
 
 func update_medal():
 	$VBoxContainer/MedalCount.text = "Medals Collected: " + str(CollectionManager.medal_amount)
